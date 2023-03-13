123fc7ed8718d5716a67874960ae434908c40597
Horst Kracman <horstkracman7134@seznam.cz>
2023-03-13 10:49:54 +0100

    Init

---
+++ CTA - Unity/Assets/Code/BoardData/ChessBoard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Code.BoardData
+{
+    public class ChessBoard : MonoBehaviour
+    {
+        IList<ChessTile> chessTiles { get; set; }
+    }
+}
